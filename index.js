d8513fdfe3e2e839d67f443db74ecfd83b2aabdc
Emmanuel Montiel Cruz <acmerev@gmail.com>
2021-11-05 11:37:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function Counter() {
   const [counter, setCounter] = useState(5);
 
   function increment() {
-    setCounter(counter+1);
+    setCounter(counter+2);
   }
   return <div>
   <p>{counter}</p>
